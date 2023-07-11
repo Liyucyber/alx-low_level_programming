21025c8fd1ac5934d6e5e5327a424666906d7253
Liyu1977 <liyu.beza.mek@gmail.com>
2023-07-11 06:22:22 -0700

    3-alloc_grid.c

--- 0x0B-malloc_free/3-main.c
+++ 0x0B-malloc_free/3-main.c
@@ -26,7 +26,7 @@ w++;
 }
 printf("\n");
 h++;
-}
+}   
 }
 
 /**
@@ -35,3 +35,18 @@ h++;
 * Return: Always 0.
 */
 int main(void)
+{
+int **grid;
+
+grid = alloc_grid(6, 4);
+if (grid == NULL)
+{
+return (1);
+}
+print_grid(grid, 6, 4);
+printf("\n");
+grid[0][3] = 98;
+grid[3][4] = 402;
+print_grid(grid, 6, 4);
+return (0);
+}
